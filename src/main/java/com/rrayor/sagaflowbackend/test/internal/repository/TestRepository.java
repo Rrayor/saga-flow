d97154138750b0eb30acd2102807e62e36e15ea8
Rrayor <rrayor.dev@gmail.com>
2023-08-06 11:28:35 +0200

    revert: :fire: Removed project files before regenerating them
    First step to remove docker and Grafana stack related stuff to focus on implementation instead
    
    BREAKING CHANGE: Nothing will work for now
    Author: rrayor

--- src/main/java/com/rrayor/sagaflowbackend/test/internal/repository/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rrayor.sagaflowbackend.test.internal.repository;
-
-import com.rrayor.sagaflowbackend.test.internal.entity.TestEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends CrudRepository<TestEntity, Long> {}
--- src/main/java/com/rrayor/sagaflowbackend/test/internal/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.rrayor.sagaflowbackend.test.internal.service;
-
-import com.rrayor.sagaflowbackend.test.internal.dto.CreateTestDto;
-import com.rrayor.sagaflowbackend.test.internal.dto.GetTestDto;
-
-public interface TestService {
-    void createTest(final CreateTestDto dto);
-    GetTestDto getTest(final Long id);
-}
